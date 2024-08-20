a6ca1334f62351d9116ee79b4dddecbfd753bd32
Sukanya228 <sukanyakadam228@gmail.com>
2024-08-20 14:38:47 +0530

    Updated Test Runner

--- src/test/java/StepDefinations/TestRunner.java
+++ src/test/java/StepDefinations/TestRunner.java
@@ -8,7 +8,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		features = "src/test/resources/Features",
 		glue = "StepDefinations",
-		tags= "Regression",
 		plugin = {"pretty","html:target/html-reports/loginreport-2.html"},
 		monochrome = true  
 
